463e3bf1f6dd010f44ccd994f0e40cd75eff41f9
Bogdan-Borunov <borunov.bogdan@gmail.com>
2024-06-15 15:41:56 +0500

    Update build_script.py

--- build_script.py
+++ build_script.py
@@ -1,10 +1,1 @@
-import os
 
-output_dir = 'public'
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
-
-with open(os.path.join(output_dir, 'index.html'), 'w') as f:
-    f.write('<html><body><h1>Hello, Netlify!</h1></body></html>')
-
-print("Build completed!")
